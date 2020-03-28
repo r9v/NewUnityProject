065114a4eb64a36dc8070df649155a588cfc5071
a <a>
2020-03-28 18:10:29 +0100

    rotate player with mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,11 +12,14 @@ public class CameraController : MonoBehaviour
 
     private float pitch = 0;
     private float yaw = 0;
+    private readonly Vector2 pitchMinMax = new Vector2(-40, 85);
 
     private void LateUpdate()
     {
         pitch -= Input.GetAxisRaw("Mouse Y") * upDownTurnSpeed * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
         yaw += Input.GetAxisRaw("Mouse X") * sidewayTurnSpeed * Time.deltaTime;
+        distanceFromPlayer -= Input.GetAxisRaw("Mouse ScrollWheel") * 20;
 
         camera.transform.eulerAngles = new Vector3(pitch, yaw);
         camera.transform.position = player.position - camera.transform.forward * distanceFromPlayer + camera.transform.up * 2;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,63 +8,23 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody player;
     public float moveSpeed = 3;
-    public float turnSpeed = 70;
-
-    private int _forwardInput = 0;
-    private int _sidewayInput = 0;
-    private int _turnInput = 0;
 
     private void Update()
     {
-        GetInput();
-        Trun();
+        var input = new Vector2(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"));
+        Move(input.normalized);
     }
 
-    private void FixedUpdate()
+    private void Move(Vector2 dir)
     {
-        Run();
-    }
-
-    private void GetInput()
-    {
-        _forwardInput = Convert.ToInt32(Input.GetAxisRaw("Vertical"));
-        _sidewayInput = Convert.ToInt32(Input.GetAxisRaw("Horizontal"));
-        if (Input.GetKey(KeyCode.E))
+        if (dir == Vector2.zero)
         {
-            _turnInput = 1;
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            _turnInput = -1;
-        }
-        else
-        {
-            _turnInput = 0;
-        }
-    }
-
-    private void Trun()
-    {
-        var turnVel = 0f;
-        if (_turnInput != 0)
-        {
-            turnVel = _turnInput * turnSpeed * Time.deltaTime;
-        }
-        player.rotation *= Quaternion.AngleAxis(turnVel, player.transform.up);
-    }
-
-    private void Run()
-    {
-        var forwardVel = Vector3.zero;
-        var sidewayVel = Vector3.zero;
-        if (_forwardInput != 0)
-        {
-            forwardVel = player.transform.forward * _forwardInput * moveSpeed;
-        }
-        if (_sidewayInput != 0)
-        {
-            sidewayVel = player.transform.right * _sidewayInput * moveSpeed;
-        }
+            player.velocity = Vector3.zero;
+            return;
+        };
+        player.transform.rotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        var forwardVel = player.transform.forward * dir.x * moveSpeed;
+        var sidewayVel = player.transform.right * dir.y * moveSpeed;
         player.velocity = sidewayVel + forwardVel;
     }
 }
