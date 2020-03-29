de9d101119a8bc21d2a8a949c4da963f20be3aaf
a <a>
2020-03-29 15:42:51 +0200

    refactor

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,16 +13,25 @@ public class CameraController : MonoBehaviour
     private float pitch = 0;
     private float yaw = 0;
     private readonly Vector2 pitchMinMax = new Vector2(-40, 85);
+    private readonly Vector2 zoomMinMax = new Vector2(0, 500);
 
     private void LateUpdate()
     {
-        pitch -= Input.GetAxisRaw("Mouse Y") * upDownTurnSpeed * Time.deltaTime;
-        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
-        yaw += Input.GetAxisRaw("Mouse X") * sidewayTurnSpeed * Time.deltaTime;
-        distanceFromPlayer -= Input.GetAxisRaw("Mouse ScrollWheel") * 20;
+        var mouseInput = MouseInput();
+        pitch = Mathf.Clamp(pitch - mouseInput.x, pitchMinMax.x, pitchMinMax.y);
+        yaw += mouseInput.y;
+        distanceFromPlayer = Mathf.Clamp(distanceFromPlayer - mouseInput.z,
+            zoomMinMax.x, zoomMinMax.y);
 
         camera.transform.eulerAngles = new Vector3(pitch, yaw);
         camera.transform.position = player.position - camera.transform.forward * distanceFromPlayer + camera.transform.up * 2;
         EventSystem.Instance.PlayerCameraRotated(camera.transform.eulerAngles.y);
+    }
+
+    private Vector3 MouseInput()
+    {
+        return new Vector3(Input.GetAxisRaw("Mouse Y") * upDownTurnSpeed * Time.deltaTime,
+            Input.GetAxisRaw("Mouse X") * sidewayTurnSpeed * Time.deltaTime,
+            Input.GetAxisRaw("Mouse ScrollWheel") * 20);
     }
 }
