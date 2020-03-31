85cff56c4590aba62a0ab11116b81cea6541e33e
a <a>
2020-03-31 21:09:53 +0200

    refactor

--- Assets/Scripts/RubiksCube.cs
+++ Assets/Scripts/RubiksCube.cs
@@ -4,21 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Axis
+{
+    X, Y, Z
+}
+
 public class RubiksCube : MonoBehaviour
 {
     public GameObject cubePrefab;
     public GameObject manipulationAreaPrefab;
     public GameObject cubeMap;
 
-    private bool _rotating;
     private readonly uint CUBE_SIZE = 3;
     private GameObject[,,] _qubies = new GameObject[3, 3, 3];
+    private CubeRotator _cubeRotator;
 
-    private void Start()
+    private void Awake()
     {
         Instantiate(manipulationAreaPrefab, transform);
-        MakeCube();
+        CreateQubies();
+        _cubeRotator = new CubeRotator(_qubies, transform.position, transform.right, transform.up, transform.forward);
         ColorCubeMap();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            _cubeRotator.Rotate(0, Axis.Z);
+        }
     }
 
     private void ColorCubeMap()
@@ -54,9 +68,9 @@ public class RubiksCube : MonoBehaviour
 
     private void ColorCubeMapFace(string faceName, int x, int y, Color color)
     {
-        var cubeMapFace = cubeMap.transform.Find(faceName);
-        cubeMapFace.GetChild((int)(x * CUBE_SIZE) + y)
-            .GetComponent<Image>().color = color;
+        var face = cubeMap.transform.Find(faceName);
+        var pixel = face.GetChild((int)(x * CUBE_SIZE) + y);
+        pixel.GetComponent<Image>().color = color;
     }
 
     private bool IsInternalQb(int x, int y, int z)
@@ -67,15 +81,7 @@ public class RubiksCube : MonoBehaviour
         return true;
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown("space"))
-        {
-            Rotate(0, Axis.Z);
-        }
-    }
-
-    private void MakeCube()
+    private void CreateQubies()
     {
         var cubePrefabSize = GetObjectBound(cubePrefab).size;
         for (var x = 0; x < CUBE_SIZE; x++)
@@ -84,7 +90,6 @@ public class RubiksCube : MonoBehaviour
             {
                 for (var z = 0; z < CUBE_SIZE; z++)
                 {
-                    //todo: use CUBE_SIZE here
                     var offset = (CUBE_SIZE - 1f) / 2f;
                     var pos = new Vector3((x - offset) * cubePrefabSize.x,
                         (y - offset) * cubePrefabSize.y,
@@ -98,7 +103,46 @@ public class RubiksCube : MonoBehaviour
         }
     }
 
-    private void Rotate(int slice, Axis axis, bool clockwise = true)
+    //get rid of this
+    private Bounds GetObjectBound(GameObject go)
+    {
+        MeshRenderer[] mfs = go.GetComponentsInChildren<MeshRenderer>();
+
+        if (mfs.Length > 0)
+        {
+            Bounds b = mfs[0].bounds;
+            for (int i = 1; i < mfs.Length; i++)
+            {
+                b.Encapsulate(mfs[i].bounds);
+            }
+            return b;
+        }
+        else
+            return new Bounds();
+    }
+}
+
+public class CubeRotator
+{
+    private bool _rotating;
+
+    private GameObject[,,] _qubies;
+
+    private Vector3 _pivot;
+    private Vector3 _right;
+    private Vector3 _up;
+    private Vector3 _forward;
+
+    public CubeRotator(GameObject[,,] qubies, Vector3 pivot, Vector3 right, Vector3 up, Vector3 forward)
+    {
+        _qubies = qubies;
+        _pivot = pivot;
+        _right = right;
+        _up = up;
+        _forward = forward;
+    }
+
+    public void Rotate(int slice, Axis axis, bool clockwise = true)
     {
         if (_rotating) return;
         _rotating = true;
@@ -123,61 +167,23 @@ public class RubiksCube : MonoBehaviour
     {
         var dir = clockwise ? 1 : -1;
         for (int y = 0; y < _qubies.GetLength(1); y++)
-        {
             for (int z = 0; z < _qubies.GetLength(2); z++)
-            {
-                _qubies[slice, y, z].transform.RotateAround(transform.position,
-                   transform.right, dir * 90);
-            }
-        }
+                _qubies[slice, y, z].transform.RotateAround(_pivot, _right, dir * 90);
     }
 
     private void RotateY(int slice, bool clockwise = true)
     {
         var dir = clockwise ? 1 : -1;
         for (int x = 0; x < _qubies.GetLength(0); x++)
-        {
             for (int z = 0; z < _qubies.GetLength(2); z++)
-            {
-                _qubies[x, slice, z].transform.RotateAround(transform.position,
-                   transform.up, dir * 90);
-            }
-        }
+                _qubies[x, slice, z].transform.RotateAround(_pivot, _up, dir * 90);
     }
 
     private void RotateZ(int slice, bool clockwise = true)
     {
         var dir = clockwise ? 1 : -1;
         for (int y = 0; y < _qubies.GetLength(1); y++)
-        {
             for (int x = 0; x < _qubies.GetLength(0); x++)
-            {
-                _qubies[x, y, slice].transform.RotateAround(transform.position,
-                  -transform.forward, dir * 90);
-            }
-        }
+                _qubies[x, y, slice].transform.RotateAround(_pivot, -_forward, dir * 90);
     }
-
-    //get rid of this
-    private Bounds GetObjectBound(GameObject go)
-    {
-        MeshRenderer[] mfs = go.GetComponentsInChildren<MeshRenderer>();
-
-        if (mfs.Length > 0)
-        {
-            Bounds b = mfs[0].bounds;
-            for (int i = 1; i < mfs.Length; i++)
-            {
-                b.Encapsulate(mfs[i].bounds);
-            }
-            return b;
-        }
-        else
-            return new Bounds();
-    }
-}
-
-public enum Axis
-{
-    X, Y, Z
 }
