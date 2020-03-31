e12731ec62a039b453ca61bfb329f90a07ceaad7
a <a>
2020-03-31 18:04:11 +0200

    stuff

--- Assets/Scripts/RubiksCube.cs
+++ Assets/Scripts/RubiksCube.cs
@@ -1,32 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RubiksCube : MonoBehaviour
 {
     public GameObject cubePrefab;
     public GameObject manipulationAreaPrefab;
+    public GameObject cubeMap;
 
+    private bool _rotating;
     private readonly uint CUBE_SIZE = 3;
-    private Side[,,] state;
-
-    private void InitState()
-    {
-        state = new Side[6, CUBE_SIZE, CUBE_SIZE];
-        for (var s = 0; s < 6; s++)
-            for (var i = 0; i < CUBE_SIZE; i++)
-                for (var j = 0; j < CUBE_SIZE; j++)
-                    state[s, i, j] = (Side)s;
-    }
-
-    private GameObject[,,] qubies = new GameObject[3, 3, 3];
-
-    private bool rotating;
+    private List<Face> _faces = new List<Face>(6);
+    private GameObject[,,] _qubies = new GameObject[3, 3, 3];
 
     private void Start()
     {
         InitState();
-        var ma = Instantiate(manipulationAreaPrefab, transform);
+        Instantiate(manipulationAreaPrefab, transform);
 
         var cubePrefabSize = GetObjectBound(cubePrefab).size;
 
@@ -43,10 +35,36 @@ public class RubiksCube : MonoBehaviour
                     var rot = Quaternion.identity;
                     var qb = Instantiate(cubePrefab, pos, rot, transform);
                     qb.name = "x:" + x + " y:" + y + " z:" + z;
-                    qubies[x, y, z] = qb;
+                    _qubies[x, y, z] = qb;
                 }
             }
         }
+    }
+
+    private void InitState()
+    {
+        _faces[0] = new Face(CUBE_SIZE, Pixel.T());
+        _faces[1] = new Face(CUBE_SIZE, Pixel.L());
+        _faces[2] = new Face(CUBE_SIZE, Pixel.F());
+        _faces[3] = new Face(CUBE_SIZE, Pixel.R());
+        _faces[4] = new Face(CUBE_SIZE, Pixel.B());
+        _faces[5] = new Face(CUBE_SIZE, Pixel.D());
+
+        ColorCubeMap();
+    }
+
+    private void ColorCubeMap()
+    {
+        _faces.ForEach((face) =>
+        {
+            var panel = cubeMap.transform.GetChild(s);
+            var panelChildIdx = 0;
+            for (var i = 0; i < CUBE_SIZE; i++)
+                for (var j = 0; j < CUBE_SIZE; j++)
+                {
+                    panel.GetChild(panelChildIdx++).GetComponent<Image>().color = state[s, i, j].color;
+                }
+        });
     }
 
     private void Update()
@@ -59,8 +77,8 @@ public class RubiksCube : MonoBehaviour
 
     private void Rotate(int slice, Axis axis, bool clockwise = true)
     {
-        if (rotating) return;
-        rotating = true;
+        if (_rotating) return;
+        _rotating = true;
         switch (axis)
         {
             case Axis.X:
@@ -75,17 +93,17 @@ public class RubiksCube : MonoBehaviour
                 RotateZ(slice, clockwise);
                 break;
         }
-        rotating = false;
+        _rotating = false;
     }
 
     private void RotateX(int slice, bool clockwise = true)
     {
         var dir = clockwise ? 1 : -1;
-        for (int y = 0; y < qubies.GetLength(1); y++)
+        for (int y = 0; y < _qubies.GetLength(1); y++)
         {
-            for (int z = 0; z < qubies.GetLength(2); z++)
+            for (int z = 0; z < _qubies.GetLength(2); z++)
             {
-                qubies[slice, y, z].transform.RotateAround(transform.position,
+                _qubies[slice, y, z].transform.RotateAround(transform.position,
                    transform.right, dir * 90);
             }
         }
@@ -94,11 +112,11 @@ public class RubiksCube : MonoBehaviour
     private void RotateY(int slice, bool clockwise = true)
     {
         var dir = clockwise ? 1 : -1;
-        for (int x = 0; x < qubies.GetLength(0); x++)
+        for (int x = 0; x < _qubies.GetLength(0); x++)
         {
-            for (int z = 0; z < qubies.GetLength(2); z++)
+            for (int z = 0; z < _qubies.GetLength(2); z++)
             {
-                qubies[x, slice, z].transform.RotateAround(transform.position,
+                _qubies[x, slice, z].transform.RotateAround(transform.position,
                    transform.up, dir * 90);
             }
         }
@@ -107,17 +125,18 @@ public class RubiksCube : MonoBehaviour
     private void RotateZ(int slice, bool clockwise = true)
     {
         var dir = clockwise ? 1 : -1;
-        for (int y = 0; y < qubies.GetLength(1); y++)
+        for (int y = 0; y < _qubies.GetLength(1); y++)
         {
-            for (int x = 0; x < qubies.GetLength(0); x++)
+            for (int x = 0; x < _qubies.GetLength(0); x++)
             {
-                qubies[x, y, slice].transform.RotateAround(transform.position,
+                _qubies[x, y, slice].transform.RotateAround(transform.position,
                   -transform.forward, dir * 90);
             }
         }
     }
 
-    public Bounds GetObjectBound(GameObject go)
+    //get rid of this
+    private Bounds GetObjectBound(GameObject go)
     {
         MeshRenderer[] mfs = go.GetComponentsInChildren<MeshRenderer>();
 
@@ -140,7 +159,68 @@ public enum Axis
     X, Y, Z
 }
 
-public enum Side
+public class Face
 {
-    U, L, F, R, B, D
+    public List<Pixel> pixels;
+    private uint _size;
+
+    public Face(uint size, Pixel startingPixel)
+    {
+        pixels = new List<Pixel>((int)size);
+
+        for (var i = 0; i < size; i++)
+        {
+            pixels.Add(startingPixel.Clone());
+        }
+
+        _size = size;
+    }
+}
+
+public class Pixel
+{
+    private string _code;
+
+    public readonly Color color;
+
+    private Pixel(string code, Color color)
+    {
+        _code = code;
+        this.color = color;
+    }
+
+    public static Pixel T()
+    {
+        return new Pixel("T", Color.red);
+    }
+
+    public static Pixel R()
+    {
+        return new Pixel("R", new Color32(255, 164, 15, 255));
+    }
+
+    public static Pixel F()
+    {
+        return new Pixel("F", Color.blue);
+    }
+
+    public static Pixel L()
+    {
+        return new Pixel("L", Color.white);
+    }
+
+    public static Pixel B()
+    {
+        return new Pixel("B", Color.green);
+    }
+
+    public static Pixel D()
+    {
+        return new Pixel("D", Color.yellow);
+    }
+
+    public Pixel Clone()
+    {
+        return new Pixel(_code, new Color(color.r, color.g, color.b, color.a));
+    }
 }
